9a32a4c0487c03e195728ce6b09ed1abe99f3ee3
varrielgd <varrielgd@users.noreply.github.com>
2026-09-10 16:55:58 +0700

    Show Yuli Teguh Hidayat author photo

--- src/data/authorsData.ts
+++ src/data/authorsData.ts
@@ -312,7 +312,8 @@ const AUTHOR_PHOTO_BY_NAME: Record<string, string> = {
   'andi banua adams': '/images/authors/andi-banua-adams.png',
   'henry dianto p sinaga': '/images/authors/henry-dianto-p-sinaga.png',
   'joko purnomo raharjo': '/images/authors/joko-purnomo-raharjo.png',
-  'dr wahyu widodo ak ca s h m si': '/images/authors/wahyu-widodo.png'
+  'dr wahyu widodo ak ca s h m si': '/images/authors/wahyu-widodo.png',
+  'dr yuli teguh hidayat sst mm': '/images/authors/yuli-teguh-hidayat.png'
 };
 
 const authorNameKey = (name: string): string => name
